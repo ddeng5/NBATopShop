4f66680bb790b1ffb95204595cc6c868a9383ba7
Marco Martin <marcomartin@Marcos-MacBook-Pro.local>
2021-03-22 22:15:50 -0400

    added api to db and create db functionaltiy

--- dbManager.py
+++ dbManager.py
@@ -4,7 +4,11 @@ import os
 from schemas import table_setup_sql
 
 DB_FILE = 'topShopDB.db'
-PLAYER_TABLE = 'Players'
+PLAYERS_TABLE = 'Players'
+SETS_TABLE = 'Sets'
+MOMENTS_TABLE = 'Moments'
+USERS_TABLE = 'Users'
+TRANSACTIONS_TABLE = 'Transactions'
 
 def open_connection():
     """ create a database connection to a SQLite database """
@@ -20,15 +24,53 @@ def open_connection():
     return conn
 
 
-def addPlayer(playerObjs):
+def addPlayers(playerObjs):
+	# playerObjs is a tuple of players in the format
+	# [player_id, name, guid]
 	conn = open_connection()
-	# purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
- 	#          ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
- 	#          ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
- 	#         ]
 	if conn:
 		cur = conn.cursor()
-		cur.executemany('INSERT INTO '+PLAYER_TABLE+' VALUES (?,?,?)', playerObjs)
+		cur.executemany('INSERT INTO '+PLAYERS_TABLE+' VALUES (?,?,?)', playerObjs)
+		conn.commit()
+		conn.close()
+
+def addSets(setObjs):
+	# setObjs is a tuple of players in the format
+	# [setId, name, category, seriesNumber, guid]
+	conn = open_connection()
+	if conn:
+		cur = conn.cursor()
+		cur.executemany('INSERT INTO '+SETS_TABLE+' VALUES (?,?,?,?,?)', setObjs)
+		conn.commit()
+		conn.close()
+		
+def addMoments(momentObjs):
+	# momentObjs is a tuple of players in the format
+	# [momentId, name, limited, circulationCount, playerId, setId, guid]
+	conn = open_connection()
+	if conn:
+		cur = conn.cursor()
+		cur.executemany('INSERT INTO '+MOMENTS_TABLE+' VALUES (?,?,?,?,?,?,?)', momentObjs)
+		conn.commit()
+		conn.close()
+		
+def addUsers(userObjs):
+	# userObjs is a tuple of players in the format
+	# [userId, username, guid]
+	conn = open_connection()
+	if conn:
+		cur = conn.cursor()
+		cur.executemany('INSERT INTO '+USERS_TABLE+' VALUES (?,?,?)', userObjs)
+		conn.commit()
+		conn.close()
+		
+def addTransactions(transactionObjs):
+	# transactionObjs is a tuple of players in the format
+	# [transactionId, momentId, buyerId, sellerId, price, updated]
+	conn = open_connection()
+	if conn:
+		cur = conn.cursor()
+		cur.executemany('INSERT INTO '+TRANSACTIONS_TABLE+' VALUES (?,?,?,?,?,?)', transactionObjs)
 		conn.commit()
 		conn.close()
 
@@ -58,6 +100,17 @@ def setup():
 	
 if __name__ == '__main__':
 	setup()
-	addPlayer([(0, 'asdlkjasdf', 'LeBron James'),
-		(1, 'lkjhkjhk', 'Kobe')])
+	addPlayers([(0, 'LeBron James', 'asdlkjasdf'),
+		(1, 'Kobe', 'lkjhkjhk')])
 
+	addSets([(0, 'Series 1', 'Shoot', 1, 'asdfkjh'),
+		(1, 'Series 2', 'Save', 2, 'asdjkjhfkjh')])
+
+	addMoments([(0, '3 pointer', True, 5000, 0, 0, 'jhfkjoskljh'),
+		(1, 'Save', False, 500000, 1, 1, 'jhflkkoskljh')])
+
+	addUsers([(0, 'coolGuy69', 'sdlkjasd'),
+		(1, 'saveFace420', 'oookjhkhk')])
+	
+	addTransactions([(0, 0, 0, 1, 500.45, '2021-03-02 19:23:12'),
+		(1, 1, 1, 0, 79.13, '2021-01-02 09:34:11')])
--- schemas.py
+++ schemas.py
@@ -1,18 +1,43 @@
 def table_setup_sql():
 	return [
 	""" CREATE TABLE IF NOT EXISTS Players (
-                    id integer PRIMARY KEY,
-                    guid integer NOT NULL,
-                    name text NOT NULL
+                    playerId integer PRIMARY KEY,
+                    name text NOT NULL,
+                    guid text
                 ); """,
-     """CREATE TABLE IF NOT EXISTS Moments (
-	                id integer PRIMARY KEY,
+    """CREATE TABLE IF NOT EXISTS Sets (
+	                setId integer PRIMARY KEY,
 	                name text NOT NULL,
-	                priority integer,
-	                status_id integer NOT NULL,
-	                project_id integer NOT NULL,
-	                begin_date text NOT NULL,
-	                end_date text NOT NULL,
-	                FOREIGN KEY (project_id) REFERENCES projects (id)
+	                category text NOT NULL,
+	                seriesNumber integer NOT NULL,
+	                guid text
+	            );""",
+     """CREATE TABLE IF NOT EXISTS Moments (
+	                momentId integer PRIMARY KEY,
+	                name text NOT NULL,
+	                limited boolean NOT NULL,
+	                circulationCount integer NOT NULL,
+	                playerId integer NOT NULL,
+	                setId integer NOT NULL,
+	                guid text,
+	                FOREIGN KEY(playerId) REFERENCES Players(playerId),
+	                FOREIGN KEY(setId) REFERENCES Sets(setId)
+	            );""",
+	 """CREATE TABLE IF NOT EXISTS Users (
+	                userId integer PRIMARY KEY,
+	                username text NOT NULL,
+	                guid text
+	            );""",
+	 """CREATE TABLE IF NOT EXISTS Transactions (
+	                transactionId integer PRIMARY KEY,
+	                momentId integer NOT NULL,
+	                buyerId integer NOT NULL,
+	                sellerId integer NOT NULL,
+	                price decimal NOT NULL,
+	                updated date NOT NULL NOT NULL,
+	                FOREIGN KEY(momentId) REFERENCES Moments(momentId),
+	                FOREIGN KEY(buyerId) REFERENCES Users(userId),
+	                FOREIGN KEY(sellerId) REFERENCES Users(userId)
 	            );"""
+	            
        ]
